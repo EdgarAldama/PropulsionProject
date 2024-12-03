7f6010de01d2730bc38401ea51c65844aa54c935
AshtonVandiver <ashtonvandiver@gmail.com>
2024-12-03 14:05:09 -0600

    Freestream Properties

--- CycleAnalysis.m
+++ CycleAnalysis.m
@@ -2,9 +2,17 @@
 
 C = Constants
 
+M0 = C.M0
+
 %% Design Point Analysis
 % Alt. Properties
-[~, T0, P0, rho0, g0, ~, ~, ~, a0] = AtmModel(C.Alt)
+[~, T0, P0, rho0, g0, ~, ~, ~, a0] = AtmModel(C.Alt);
+R = 287.052874 % J/(kg*K)
+gamma = 1.4
+
+%% Freestream
+Tt0 = T0 * (1+(gamma-1)/2 * M0^2); % Calculate Total Temp
+Pt0 = P0 * (1+(gamma-1)/2 * M0^2)^(gamma/(gamma-1)); % Calculate Total Pressure
 
 %% Inlet
 
